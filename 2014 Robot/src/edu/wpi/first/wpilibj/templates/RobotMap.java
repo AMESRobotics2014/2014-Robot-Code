0f4b35e84e1ec15fe39e93be7f8ab0503063bd5c
Kolton Yager <>
2014-01-12 10:00:50 -0700

    Added comment description to RobotMap, will add descriptions to other classes soon.

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -1,15 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class should be used to store global variables and value. Sub-classes in this class should almost always
+ * be used as a tool for structuring data and treating certain elements of the code as objects. The only methods
+ * in this class should be for data managment, and maybe a few for robot organization, and control, aka nothing complex
+ * or related to the control of the hardware.
  */
 
 package edu.wpi.first.wpilibj.templates;
+//An example of global values that should be stored in this class are pin assignments.
 
-/**
- *
- * @author AliNazzal
- */
 public class RobotMap {
     
 }
