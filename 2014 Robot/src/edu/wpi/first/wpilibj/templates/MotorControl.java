2fe25ba34096f36bb7f9b327f919a3331507bc83
Kolton Yager <>
2014-01-12 13:13:52 -0700

    Renamed some classes

---
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/MotorControl.java
@@ -0,0 +1,7 @@
+/*This class is where all calls to the actual motor hardware should occur, they should be methods callable from the main function.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+public class MotorControl {
+    
+}
--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -24,7 +24,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class RobotTemplate extends IterativeRobot {
+public class RobotMain extends IterativeRobot {
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
