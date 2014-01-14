116814904410953766acd3e29425193935ed883f
AliNazzal <AliNazzal@192.168.1.4>
2014-01-13 21:07:34 -0700

    Minor additions.

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/ImageProcessing.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/ImageProcessing.java
@@ -5,6 +5,7 @@
  */
 
 package edu.wpi.first.wpilibj.templates;
+import java.lang.Math.*;
 
 /**
  *
@@ -13,5 +14,15 @@ package edu.wpi.first.wpilibj.templates;
 
 
 public class ImageProcessing {
+   
+    /** 
+     * The view angle and image resolution need to be in compliance with the camera.
+     */
+    public final int Y_IMAGE_RES = 480; // Need to check if this is resolution on camera.
+    public final double VIEW_ANGLE = 37.4; // For the M1011 Axis Camera - need to check model of camera.
+    public final double PI = Math.PI;
+    
+    // Score limits.
+    
     
 }
