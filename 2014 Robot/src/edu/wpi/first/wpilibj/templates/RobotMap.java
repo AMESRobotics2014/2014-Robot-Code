1b1c0666a4d78fa5833f1148514b2fa0dbc7e79e
Tarun <>
2014-01-13 17:02:26 -0700

    Names

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -11,5 +11,10 @@ package edu.wpi.first.wpilibj.templates;
  * @author AliNazzal
  */
 public class RobotMap {
+     private static final double PI= 3.1415926535897932384626433832795028841;
+     private static final double View_Angle=43.5;
+     private static final int Image_Res_Y= 480;
+          private static final int Image_Res_4= 480;
+     
     
 }
