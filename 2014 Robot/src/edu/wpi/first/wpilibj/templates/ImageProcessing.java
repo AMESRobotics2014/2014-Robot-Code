0043e35c13e410f0c73f7683863da1a6290dd186
Kolton Yager <>
2014-01-19 15:04:48 -0700

    Jonah Paralax code pieces

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/ImageProcessing.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/ImageProcessing.java
@@ -1,12 +1,62 @@
-/*Class for handeling massive amounts of image processing code, this may actually end up holding very little if the Raspberry pie is used instead.
- */
+
 
 package edu.wpi.first.wpilibj.templates;
 
-/**
- *
- * @author AliNazzal, Mark, & TARUN
- */
 public class ImageProcessing {
-    
+    /**
+     * 
+     * @param semiBaseline half the distance between cameras
+     * @param leftAngle the horrizantal angle in the left camera
+     * @param rightAngle the horrizantal angle in the right camera
+     * @return  the distance directly forward from a point halfway in between both cameras
+     */
+    public static double getDistance(double semiBaseline, double leftAngle, double rightAngle)  {
+        if(rightAngle==90)   {
+            return semiBaseline*2*Math.tan(leftAngle/180*Math.PI);
+        }
+        if(leftAngle==90)   {
+            return semiBaseline*-2*Math.tan(rightAngle/180*Math.PI);
+        }
+        double x=(semiBaseline*Math.tan(rightAngle/180*Math.PI)-semiBaseline*Math.tan(leftAngle/180*Math.PI))/
+                (Math.tan(leftAngle/180*Math.PI)-Math.tan(rightAngle/180*Math.PI));
+        return (x+semiBaseline)*Math.tan(leftAngle/180*Math.PI);
+    }
+    /**
+     * 
+     * @param semiBaseline half the distance between the cameras
+     * @param leftAngle the horrizantal angle in the left camera
+     * @param rightAngle the horrizantal angle in the right camera
+     * @return the distance to the right from a point halfway in between the cameras
+     */
+    public static double getOffset(double semiBaseline, double leftAngle, double rightAngle)  {
+        if(rightAngle==90)   {
+            return semiBaseline;
+        }
+        if(leftAngle==90)   {
+            return -semiBaseline;
+        }
+        return (semiBaseline*Math.tan(rightAngle/180*Math.PI)-semiBaseline*Math.tan(leftAngle/180*Math.PI))/
+                (Math.tan(leftAngle/180*Math.PI)-Math.tan(rightAngle/180*Math.PI));
+    }
+    /**
+     * 
+     * @param semiBaseline half the distance between the cameras
+     * @param leftAngle the horrizantal angle in the left camera
+     * @param rightAngle the horrizantal angle in the right camera
+     * @param declination the declination of the object
+     * @return the elevation above the cameras
+     */
+    public static double getHeight(double semiBaseline, double leftAngle, double rightAngle, double declination)  {
+        double y=0;
+        if(rightAngle==90)   {
+            y=semiBaseline*2*Math.tan(leftAngle/180*Math.PI);
+        }
+        if(leftAngle==90)   {
+            y=semiBaseline*-2*Math.tan(rightAngle/180*Math.PI);
+        }
+        double x=(semiBaseline*Math.tan(rightAngle/180*Math.PI)-semiBaseline*Math.tan(leftAngle/180*Math.PI))/
+                (Math.tan(leftAngle/180*Math.PI)-Math.tan(rightAngle/180*Math.PI));
+        y=(x+semiBaseline)*Math.tan(leftAngle/180*Math.PI);
+        return y*Math.tan(declination/180*Math.PI);
+    }
 }
