76042e9904a130b750e9d6a6037eedd512eb2dc9
BenRose <>
2014-01-29 17:10:34 -0700

    Logical syntax fixes!

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/MotorControl.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/MotorControl.java
@@ -4,87 +4,83 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Jaguar;
 
 /**
- * This class is where all calls to the actual motor hardware should occur. 
- * They should be methods callable from the main function.
+ * This class is where all calls to the actual motor hardware should occur. They
+ * should be methods callable from the main function.
+ *
  * @author kolton.yager
  */
-
 public class MotorControl {
-    
+
     protected static Victor firstRightMotor;
     protected static Victor secondRightMotor;
-    
     protected static Victor firstLeftMotor;
     protected static Victor secondLeftMotor;
-    
     protected static Jaguar shooterLeftMotor;
     protected static Jaguar shooterRightMotor;
-    
     protected static Victor grabberMotor;
-    
     protected static Victor elevatorMotor;
-    
+
     public void init() {
         firstRightMotor = new Victor(RobotMap.firstRightMotor);
         secondRightMotor = new Victor(RobotMap.secondRightMotor);
-        
+
         firstLeftMotor = new Victor(RobotMap.firstLeftMotor);
         secondLeftMotor = new Victor(RobotMap.secondLeftMotor);
-        
+
         shooterLeftMotor = new Jaguar(RobotMap.shooterLeftMotor);
         shooterRightMotor = new Jaguar(RobotMap.shooterRightMotor);
-        
+
         grabberMotor = new Victor(RobotMap.grabberMotor);
-        
+
         elevatorMotor = new Victor(RobotMap.elevatorMotor);
     }
-    
+
     public void drive(double[] mv) {
         firstRightMotor.set(limit(mv[0]));
         secondRightMotor.set(limit(mv[0]));
-        
+
         firstLeftMotor.set(limit(mv[1]));
         secondLeftMotor.set(limit(mv[1]));
     }
-    
+
     public void stopDrive() {
         firstRightMotor.set(0);
         secondRightMotor.set(0);
-        
+
         firstLeftMotor.set(0);
         secondLeftMotor.set(0);
     }
-    
+
     public static double limit(double val) {
-        if (val < -1)
+        if (val < -1) {
             val = -1;
-        
-        if (val > 1)
+        }
+
+        if (val > 1) {
             val = 1;
-        
+        }
+
         return val;
     }
-    
-    public void shooter(){
-        
+
+    public void shooter() {
     }
-    
-    public void grabber(){
-        
+
+    public void grabber() {
     }
-    
-    public void elevator(double val, boolean Button1, boolean Button2, boolean autoAim){
-       if(val == 0){
-           while(autoAim = false){
-           elevatorMotor.set(1);
-           if(Button1 = true){
-               elevatorMotor.set(-1);
-           }
-           }
-       }else{
-           while(Button2 = false){
-           elevatorMotor.set(-1);
-         }
-       }
+
+    public void elevator(double val, boolean Button1, boolean Button2, boolean autoAim) {
+        if (val == 0) {
+            if (autoAim == false) {
+                elevatorMotor.set(1);
+                if (Button1 == true) {
+                    elevatorMotor.set(-1);
+                }
+            }
+        } else {
+            while (Button2 = false) {
+                elevatorMotor.set(-1);
+            }
+        }
     }
 }
