91cd1ac09c76de74c45cb98d752e1660146ae5f7
Kolton Yager <>
2014-02-01 11:50:36 -0700

    Added timer to main

--- 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ 2014 Robot/src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -29,22 +29,118 @@ import edu.wpi.first.wpilibj.Watchdog;
  */
 public class RobotMain extends IterativeRobot {
 
-    /**
-     * Drive Settings
-     */
-    // RobotDrive drive;
-    /**
-     * Left Joystick Parameters
-     */
-    // Joystick leftstick;
-    /**
-     * Right Joystick Parameters
-     */
-    // Joystick rightstick;
-    MotorControl MC;
+    MotorControl MC;package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Timer;
+import java.util.Vector;
+
+public class MasterTimer extends Timer {
+
+    Vector actindex;
+    accessdata use;
+    public void MasterTimer() {
+        actindex = new Vector();
+        addEventTimer("TurboTimer");
+        addEventTimer("DEBUGprints");
+        addEventTimer("ShiftTimer");
+    }
+    
+      public void Init() {
+        actindex = new Vector();
+        addEventTimer("TurboTimer");
+        addEventTimer("DEBUGprints");
+        addEventTimer("ShiftTimer");
+    }
+
+    public void addEventTimer(String tid) {
+        use = new accessdata(tid);
+        use.Init(tid);
+        actindex.addElement(use);
+    }
+
+    public void listIndicesDEBUG() {
+        try{
+        int i;
+        System.out.println("Test" + actindex.size());
+        for (i = 0; i < actindex.size(); i++) {
+           use = (accessdata)actindex.elementAt(i);
+            System.out.println("Name: " + use.id + " at index location:[" + i + "]");
+        }
+        }catch(NullPointerException ex){
+            System.out.println("This index doesn't exist D:");
+        }
+    }
+    
+    public double gdt(int loc){
+        System.out.println("Getting gdt at location:" +loc);
+        use = (accessdata)actindex.elementAt(loc);
+        System.out.println(use.gdt());
+        return use.gdt();
+    }
+
+    public void Freset() {//Full reset!!!
+        this.reset();
+        int i;
+        for (i = 0; i < actindex.size(); i++) {
+            use = (accessdata)actindex.elementAt(i);
+            use.wipe();
+        }
+    }
+
+    public void Ereset(int[] exclude) {//Exclusion reset!!!
+        int i;
+        for (i = 0; i < actindex.size()-1; i++) {
+            use = (accessdata)actindex.elementAt(i);
+            for (int k = 0; k < exclude.length; k++) {
+                if (i == exclude[k]) {
+                    break;
+                } else {
+                    use.wipe();
+                }
+            }
+        }
+    }
+
+    public void Sreset(int[] include) {//Selective reset
+        int i;
+        for (i = 0; i < include.length; i++) {
+            use = (accessdata)actindex.elementAt(i);
+            use.wipe();
+        }
+    }
+
+    public class accessdata {
+
+         double ot;
+         double dt;
+         String id;
+
+        accessdata(String tid) {
+        }
+        
+        public void Init(String tid){
+            id = tid;
+            ot = get();
+            dt = -10;//Just Cause 2  
+        }
+
+        public double gdt() {
+            dt = get() - ot;
+            ot = get();
+            return dt;
+        }
+
+        public  void wipe() {
+            ot = 0;
+            dt = 0;
+            id = null;
+        }
+    }
+}
     RobotMap R;
     ImageProcessing IP;
     InputManager IM;
+    MasterTimer MT;
     protected static Watchdog wd;
     boolean turbo;
     boolean shiftSTR;
@@ -62,9 +158,6 @@ public class RobotMain extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-        // drive = new RobotDrive(1, 2);
-        // leftstick = new Joystick(1);
-        // rightstick = new Joystick(2);
         MC = new MotorControl();
         MC.init();
         wd = Watchdog.getInstance();
@@ -72,12 +165,8 @@ public class RobotMain extends IterativeRobot {
         wd.setEnabled(true);
         turbo = false;
         shiftSTR = false;
-        // Here if the code before does not call it.
-        // MC.init();
-
-        // Sim = new Simulator();
-        // RM = new RobotMap();
-
+        MT = new MasterTimer();
+        MT.Init();
         IM = new InputManager();
         IM.init();
 
@@ -106,21 +195,13 @@ public class RobotMain extends IterativeRobot {
         MC.drive(new double[]{1, 1});
         MC.elevator(1, false, false, true);//What booleans should be used?
         MC.elevator(0, false, false, true);
-        //if (IP.target.hot) {//If the goal is hot, fire
-            //MC.shooter();
-        //}
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        /*
-         while(true && isOperatorControl() && isEnabled()) {
-         drive.tankDrive(leftstick, rightstick);
-         Timer.delay(.05);
-         }
-         */
+        MT.listIndicesDEBUG();
         while (true && isOperatorControl() && isEnabled()) {
             //Undefined names are placeholders
 
@@ -134,17 +215,23 @@ public class RobotMain extends IterativeRobot {
             IM.dPadValue();
 
             // This is the driving, might get changed.
+            System.out.println("Current time of Mt is:" + MT.get());
+            if (MT.gdt(1) >= 1.0) {
+                System.out.println("I am a debug print, should happen every half second");
+            }
             MC.drive(IM.getPureAxis());
             // MC.drive(IM.rampSpeed(IM.getPureAxis()));
             MC.shooter(IM.raiseGrabber.getState());
+            if(MT.gdt(3) < .02){//adsf
             MC.transmission();
-            //MC.grabber(Switch1);
-            // If robot is running out of control.
-            /*if (IM.buttonStop.getState()) { 
+            }
+            class block{
+             /*MC.grabber(Switch1);
+             If robot is running out of control.
+             if (IM.buttonStop.getState()) { 
              MC.stopDrive();
              System.out.println("Stopped.");
-             }*/
-            /*
+             }
              double driveX = IM.getAxis(IM.LEFT_X);
              double driveY = IM.getAxis(IM.LEFT_Y);
              double aimX = IM.getAxis(IM.RIGHT_X);
@@ -155,8 +242,9 @@ public class RobotMain extends IterativeRobot {
              }
              if (IM.getButton(IM.PASS)) {
              MC.shoot(0.75); //Another placeholder
-             }
-             */
+             }*/
+            }
+            
         }
     }
 }
